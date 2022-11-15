ef048bbdff500418a52fca1ea00d2dcc2d6173b6
akasimo <email@example.com>
2022-11-15 17:35:16 +0300

    added contract calls

--- scripts/Assignment.ts
+++ scripts/Assignment.ts
@@ -2,23 +2,22 @@ import { ethers, network } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Ballot, Ballot__factory } from "../typechain-types";
 import * as dotenv from 'dotenv'
-import { BigNumber, Wallet } from "ethers";
+import { BigNumber, ContractTransaction, Wallet } from "ethers";
 import { Provider } from "@ethersproject/providers";
-
 dotenv.config()
 
 async function main() {
-    // Usage: yarn run ts-node --files .\scripts\Deployment.ts "Raspberry" "Vanilla" "Pistacchio"
+    // Usage: yarn run ts-node --files .\scripts\Assignment.ts "Raspberry" "Vanilla" "Pistacchio"
 
     const localDeploy = true;
-    const numberOfWallets = 2;
+    const numberOfWallets = 3;
 
     let accounts: Wallet[] = [];
     let provider: Provider;
-    let balance: BigNumber;
-    let balanceInEth: string;
     let minimumRequiredBalance= 0.01;
-
+    // let balance: BigNumber;
+    // let balanceInEth: string;
+    
     provider = ethers.provider;
     if (!localDeploy) {
         console.log("Using Goerli network");
@@ -27,8 +26,6 @@ async function main() {
             alchemy: process.env.ALCHEMY_API_KEY,
             etherscan: process.env.ETHERSCAN_API_KEY,
         });
-        console.log("potato");
-        return;
     }
     else {
         console.log("Using local network");
@@ -45,12 +42,12 @@ async function main() {
         if (localDeploy) {
             await network.provider.send("hardhat_setBalance", [
                 accounts[index].address,
-                "0x100000000000000",
+                "0x1000000000000000",
               ]);
         }
         
-        balance = await accounts[index].getBalance();
-        balanceInEth = ethers.utils.formatEther(balance)
+        const balance = await accounts[index].getBalance();
+        const balanceInEth = ethers.utils.formatEther(balance);
         console.log(`Account ${index} balance: ${balanceInEth} ETH`);
 
         if (Number(balanceInEth) < minimumRequiredBalance) throw new Error("Balance not enough to run the script");
@@ -84,7 +81,46 @@ async function main() {
     const chairPerson = await ballotContract.chairperson();
     console.log(`The chairperson of the ballot: ${chairPerson}`);
 
-    // # give the right to vote to the first account
+
+    // give voting rights to the other accounts
+    console.log("\nGiving voting rights to the other accounts");
+    for (let index = 1; index < numberOfWallets; index++) {
+        let tx = await ballotContract.connect(accounts[0]).giveRightToVote(accounts[index].address);
+        let receipt = await tx.wait();
+        console.log(`Account ${index} - (${accounts[index].address}) has been given voting rights`);
+        console.log(`Transaction hash: ${receipt.transactionHash}`);
+        console.log(`Tx cost: ${ethers.utils.formatEther(Number(receipt.cumulativeGasUsed) * Number(receipt.effectiveGasPrice))} ETH`);
+    }
+    
+    // delegating votes 
+    console.log("\nDelegating votes");
+    let tx = await ballotContract.connect(accounts[0]).delegate(accounts[2].address);
+    let receipt = await tx.wait();
+    console.log(`Account 0 / (${accounts[0].address}) delegated the rights to: Account 2 / (${accounts[2].address})`); 
+    console.log(`Transaction hash: ${receipt.transactionHash}`);
+    // console.log({receipt});
+    // console.log(`Tx cost: ${ethers.utils.formatEther(Number(receipt.cumulativeGasUsed) * Number(receipt.effectiveGasPrice))} ETH`);
+
+    // casting votes
+    console.log("\nCasting votes");
+
+    let voted_to = 1;
+    tx = await ballotContract.connect(accounts[1]).vote(1);
+    // console.log(`${accounts[1].address} voted for proposal ${voted_to} / ${proposals[voted_to]}`);
+    console.log(`Account 1 / (${accounts[1].address}) voted for proposal ${voted_to} / ${ethers.utils.parseBytes32String((await ballotContract.proposals(voted_to)).name)}`);
+    receipt = await tx.wait();
+    console.log(`Transaction hash: ${receipt.transactionHash}`);
+
+    voted_to = 2;
+    tx = await ballotContract.connect(accounts[2]).vote(voted_to);
+    // console.log(`${accounts[2].address} voted for proposal ${voted_to} / ${proposals[voted_to]}`);
+    console.log(`Account 2 / (${accounts[2].address}) voted for proposal ${voted_to} / ${ethers.utils.parseBytes32String((await ballotContract.proposals(voted_to)).name)}`);
+    receipt = await tx.wait();
+    console.log(`Transaction hash: ${receipt.transactionHash}`);
+    
+    // querying results
+    let winnerName = await ballotContract.winnerName();
+    console.log(`\nThe winner is: ${ethers.utils.parseBytes32String(winnerName)}`);
 }
 
 main().catch((error) => {
